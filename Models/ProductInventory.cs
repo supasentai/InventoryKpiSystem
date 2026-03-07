343086dffc2487d0feb7bd72b3dc3f2911fb91b0
baooooo <tranquocbao7a10@gmail.com>
2026-03-07 13:44:52 +0700

    Update ProductInventory.cs

--- Models/ProductInventory.cs
+++ Models/ProductInventory.cs
@@ -13,10 +13,6 @@ namespace InventoryKpiSystem.Models
         
         public decimal UnitCost { get; set; }
         
-        // Lưu trữ các ngày mua hàng để tính KPI 5 (Average Inventory Age)
         public List<DateTime> PurchaseDates { get; set; } = new List<DateTime>();
-
-        // Thuộc tính tính toán phụ trợ (Tuỳ chọn)
-        public int CurrentStock => PurchasedQuantity - SoldQuantity;
     }
 }
